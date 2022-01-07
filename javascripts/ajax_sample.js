511ac91e27e8ef6f6fe58ddc3918d2dbff691870
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-07 23:42:02 +0900

    Change: JavaScriptからjQueryに変更

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,26 +1,23 @@
-let number = 0; //--1
-const titleArea = document.getElementById("title"); //--2
-const contentArea = document.getElementById("content"); //--2
-const videoArea = document.getElementById("video"); //--2
-const button = document.getElementById('btn'); //--3
+$(function() {
+  const button = $("#btn");
+  const videoArea = $("#video");
+  const titleArea = $("#title");
+  const contentArea = $("#content");
+  let number = 0;
 
-function getData() {
-  button.addEventListener('click', e => { //--4
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState == 4) {
-        if(request.status == 200) {
-          titleArea.innerHTML = request.response[number].title; //--5
-          contentArea.innerHTML = request.response[number].content; //--5
-          videoArea.setAttribute("src", request.response[number].url); //--6
-          number == 2 ? number = 0 : number++; //--7
-        }
-      }
-    }
-    request.open("GET", "ajax.json");
-    request.responseType = "json";
-    request.send(null);
-  })
-}
+  function getData() {
+    button.click(function(){
+      $.ajax('ajax.json',{
+        type: 'GET',
+        success: function(data) {
+          titleArea.html(data[number].title);
+          contentArea.html(data[number].content);
+          videoArea.attr('src', data[number].url);
+          number == 2 ? number = 0 : number++;
+        },
+      })
+    });
+  };
 
-window.onload = getData;
+  getData();
+})
