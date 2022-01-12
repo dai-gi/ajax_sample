3af3415500a36d6769062cdb9aaa5827d73ddc81
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-13 06:08:34 +0900

    Improve: 初回のみデータを取得するよう改善中

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -6,22 +6,30 @@ const contentArea = document.getElementById("content");
 const button = document.getElementById('btn');
 
 function getData() {
-  button.addEventListener('click', e => {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState == 4) {
-        if(request.status == 200) {
-          let str = JSON.stringify(request.response);
-          console.log(str[0].title);
-        }
+  // コンストラクタ作成
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    // 下記の条件分岐は、【 if(xhr.readyState == 4 && xhr.status == 200){} 】と同義
+    if(request.readyState == 4) {
+      if(request.status == 200) {
+        const jsonObj = JSON.parse(JSON.stringify(request.response));
+        data.push(jsonObj[0]);
+        debugger
       }
     }
-    request.open("GET", "ajax.json");
-    request.responseType = "json";
-    request.send(null);
-  })
-}
+  }
+  // リクエスト
+  request.open("GET", "ajax.json");
+  request.responseType = "json";
+  // リスエスト送信
+  request.send(null);
 
+}
+console.log(data[0].title);
+debugger
+
+button.addEventListener('click', e => {
+})
 
 
 window.onload = getData;
